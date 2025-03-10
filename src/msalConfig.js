d7003205afd3184de2a9eb328d11e0486fe332d3
Muthukumar1104 <ksrmk437@gmail.com>
2025-03-10 13:39:57 +0530

    netlify URL changed

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -5,8 +5,8 @@ export const msalConfig = {
     clientId: "1dee9989-1103-4b36-a17a-14ec34339c2d",
     authority:
       "https://login.microsoftonline.com/a4eaa551-a3fd-442a-ac8c-968dbe27a805",
-    redirectUri: "http://localhost:3000",
-    // redirectUri: "https://rbac-website.netlify.app/",
+    // redirectUri: "http://localhost:3000",
+    redirectUri: "https://rbac-website.netlify.app/",
   },
   cache: {
     cacheLocation: "localStorage",
